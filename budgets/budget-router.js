9d7fcfbd0164f45f440af890a2a8c67fb0966236
schang1146 <sammychang2185@gmail.com>
2019-08-07 19:33:21 -0700

    fixed seed issue; finished GET operations

--- budgets/budget-router.js
+++ budgets/budget-router.js
@@ -5,29 +5,35 @@ const db = require("../data/dbConfig.js");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-    try {
-        const accounts = await db("accounts");
-
-        res.status(200).json(accounts);
-    } catch (err) {
-        res.status(500).json({ message: "error getting accounts", error: err });
-    }
+router.get("/", (req, res) => {
+    db.select("*")
+        .from("accounts")
+        .then(accounts => res.status(200).json(accounts))
+        .catch(err =>
+            res
+                .status(500)
+                .json({ message: "error getting accounts", error: err })
+        );
 });
 
-// router.get("/", (req, res) => {
-//     db.select("*")
-//         .from("accounts")
-//         .then(accounts => res.status(200).json(accounts))
-//         .catch(err =>
-//             res
-//                 .status(500)
-//                 .json({ message: "error getting accounts", error: err })
-//         );
-// });
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
 
-router.get("/id", (req, res) => {
-
-})
+    db.select("*")
+        .from("accounts")
+        .where("id", "=", id)
+        .then(account => {
+            if (account[0]) {
+                res.status(200).json(account[0]);
+            } else {
+                res.status(404).json({ error: `invalid id: ${id}` });
+            }
+        })
+        .catch(err =>
+            res
+                .status(500)
+                .json({ message: "error getting account", error: err })
+        );
+});
 
 module.exports = router;
