0bec03243535c071ccefcedc1721acd61a36b2c9
schang1146 <sammychang2185@gmail.com>
2019-08-07 19:52:36 -0700

    finished POST, PUT, and DELETE operations

--- budgets/budget-router.js
+++ budgets/budget-router.js
@@ -5,6 +5,20 @@ const db = require("../data/dbConfig.js");
 
 const router = express.Router();
 
+router.post("/", (req, res) => {
+    const accountInfo = req.body;
+
+    db.select("*")
+        .from("accounts")
+        .insert(accountInfo)
+        .then(res.status(201).json(accountInfo))
+        .catch(err =>
+            res
+                .status(500)
+                .json({ message: "error inserting account", error: err })
+        );
+});
+
 router.get("/", (req, res) => {
     db.select("*")
         .from("accounts")
@@ -35,5 +49,32 @@ router.get("/:id", (req, res) => {
                 .json({ message: "error getting account", error: err })
         );
 });
+
+router.put("/:id", (req, res) => {
+    const { id } = req.params;
+    const accountInfo = req.body;
+
+    db.select("*")
+        .from("accounts")
+        .where("id", "=", id)
+        .update(accountInfo)
+        .then(numUpdated => res.status(200).json({ updated: numUpdated }))
+        .catch(err =>
+            res
+                .status(500)
+                .json({ message: "error updating account", error: err })
+        );
+});
+
+router.delete("/:id", (req, res) => {
+    const { id } = req.params;
+
+    db.select("*")
+        .from("accounts")
+        .where("id", "=", id)
+        .del()
+        .then(numDeleted => res.status(204).json({ deleted: numDeleted }))
+        .catch(err => res.status(500).json({ message: "error deleting account", error: err }))
+})
 
 module.exports = router;
